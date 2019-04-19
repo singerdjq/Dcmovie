c3b941c2cc1796ff79af005fd03703297ebc36e9
singerdjq <dengjiaqinihao@gmail.com>
2019-04-19 23:46:09 +0000

    third push

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PayTypeSelector from 'PayTypeSelector'
+//import React from 'react'
+//import ReactDOM from 'react-dom'
+//import PayTypeSelector from 'PayTypeSelector'
 
 
 
-document.addEventListener('turbolinks:load', function(){
-                          var element=document.getElementById("pay-type-component");
-  ReactDOM.render(<PayTypeSelector />, element);
+//document.addEventListener('turbolinks:load', function(){
+                          //var element=document.getElementById("pay-type-component");
+  //ReactDOM.render(<PayTypeSelector />, element);
     
-});
+//});
 
